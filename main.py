90da4b07f487370bd3d8a5e66f4f936bb7b3c3f4
bebus <v.gerashchenkoua@gmail.com>
2025-04-12 21:47:05 +0300

    basic selenium implementation

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-import selenium
+import time
+
+import undetected_chromedriver as uc
+from undetected_chromedriver import ChromeOptions
+
+
+def main():
+    options = ChromeOptions()
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-gpu")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+
+    driver = uc.Chrome(headless=False,use_subprocess=False)
+
+    driver.get("https://tiktok.com")
+    time.sleep(100)
+
+if __name__ == '__main__':
+    main()
